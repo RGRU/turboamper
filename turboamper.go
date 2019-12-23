b413272357a286cc46800d3edbc30caef46e5ff1
barsuk <brudershaft@mail.ru>
2019-12-23 16:49:37 +0300

    init with Fb Vk Insta and Twitta

---
+++ turboamper.go
@@ -0,0 +1,28 @@
+// Use of this source code is governed by a MIT-style license that can be found in the LICENSE file.
+
+// Package turboamper provides support for making some HTML structured texts validateable by Yandex Turbo and Google AMP services.
+package turboamper
+
+// FbToAMP convertes given facebook embeddable html to AMP
+func FBtoAMP(htmlText []byte) ([]byte, error) {
+    converted := make([]byte, 0, len(htmlText))
+    return converted, nil
+}
+
+// VkToAMP convertes given vkontakte embeddable html to AMP
+func VKtoAMP(htmlText []byte) ([]byte, error) {
+    converted := make([]byte, 0, len(htmlText))
+    return converted, nil
+}
+
+// InstaToAMP convertes given instagram embeddable html to AMP
+func InstaToAMP(htmlText []byte) ([]byte, error) {
+    converted := make([]byte, 0, len(htmlText))
+    return converted, nil
+}
+
+// TwitToAMP convertes given instagram embeddable html to AMP
+func TwitToAMP(htmlText []byte) ([]byte, error) {
+    converted := make([]byte, 0, len(htmlText))
+    return converted, nil
+}
