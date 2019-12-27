e2fa227297afa7567d77d813ee6a4f5762f2b475
barsuk <barsuk@rg.ru>
2019-12-27 15:09:11 +0300

    ready to use fb iframe amp-fier

--- turboamper.go
+++ turboamper.go
@@ -6,12 +6,16 @@ package turboamper
 import (
 	"bytes"
 	"fmt"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"net/url"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-// VkPost contents widget data
-type VkPost struct {
+// vkPost contents widget data
+type vkPost struct {
 	OwnerId int64
 	PostId  int64
 	Hash    string
@@ -20,7 +24,7 @@ type VkPost struct {
 }
 
 // printAMP returns ready to handle AMP with given parameters
-func (vkpost *VkPost) printAMP() []byte {
+func (vkpost *vkPost) printAMP() []byte {
 	attributes := ""
 	if vkpost.Width > 0 {
 		attributes += fmt.Sprintf(` width="%d"`, vkpost.Width)
@@ -35,13 +39,92 @@ func (vkpost *VkPost) printAMP() []byte {
 	return []byte(amp)
 }
 
-// FbToAMP convertes given facebook embeddable html to AMP
-func FbToAMP(htmlText []byte) ([]byte, error) {
-	converted := make([]byte, 0, len(htmlText))
-	return converted, nil
+type fbPost struct {
+	isVideo bool
+	Width   int64
+	Height  int64
+	Href    string
+	Src     string
 }
 
-// VkToAMP convertes given vkontakte embeddable html to AMP
+// printAMP returns ready to handle AMP with given parameters
+func (fbpost *fbPost) printAMP() []byte {
+	var attributes string
+	if fbpost.Width > 0 {
+		attributes += fmt.Sprintf(` width="%d"`, fbpost.Width)
+	}
+	if fbpost.Height > 0 {
+		attributes += fmt.Sprintf(` height="%d"`, fbpost.Height)
+	}
+	if fbpost.isVideo {
+		attributes += ` data-embed-as="video"`
+	}
+	template := `<amp-facebook layout="responsive"%s data-href="%s"></amp-facebook>`
+
+	amp := fmt.Sprintf(template, attributes, fbpost.Href)
+
+	return []byte(amp)
+}
+
+// FbToAMP convertes given facebook embeddable html to AMP
+func FbToAMP(htmlText []byte) ([]byte, error) {
+	//`<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fstcnk%2Fposts%2F3384458724928901&width=500" width="500" height="498" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+	pointerNode, err := html.Parse(bytes.NewReader(htmlText))
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse fb iframe")
+	}
+	var post fbPost
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.DataAtom == atom.Iframe {
+			for _, iframe := range n.Attr {
+				switch iframe.Key {
+				case "src":
+					post.Src = iframe.Val
+				case "width":
+					w, err := strconv.ParseInt(iframe.Val, 10, 0)
+					if err == nil {
+						post.Width = w
+					}
+				case "height":
+					h, err := strconv.ParseInt(iframe.Val, 10, 0)
+					if err == nil {
+						post.Height = h
+					}
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(pointerNode)
+
+	if !(len(post.Src) > 0) {
+		return nil, fmt.Errorf("no src in the url")
+	}
+
+	urlPtr, err := url.Parse(post.Src)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse fb url")
+	}
+
+	if !strings.Contains(urlPtr.Hostname(), "facebook.com") {
+		return nil, fmt.Errorf("it is not facebook url")
+	}
+
+	if strings.Contains(urlPtr.Path, "video.php") {
+		post.isVideo = true
+	}
+
+	post.Href = urlPtr.Query().Get("href")
+
+	return post.printAMP(), nil
+}
+
+// VkToAMP convertes given vkontakte widget post to AMP
+// What is that? Look https://vk.com/dev/widget_post
 func VkToAMP(htmlText []byte) ([]byte, error) {
 	// VK.Widgets.Post("vk_post_1_45616", 1, 45616, 'ZMk4b98xpQZMJJRXVsL1ig', {width: 500})
 	//VK.Widgets.Post("vk_post_-175249128_1156", -175249128, 1156, 'HmCFKRSM81NEzJ8mY9gzgXOlEFM')
@@ -70,7 +153,7 @@ func VkToAMP(htmlText []byte) ([]byte, error) {
 		return nil, fmt.Errorf("cannot parse owner id")
 	}
 
-	data := &VkPost{OwnerId: ownerId, PostId: postId, Hash: string(widgetParsed[5])}
+	data := &vkPost{OwnerId: ownerId, PostId: postId, Hash: string(widgetParsed[5])}
 
 	// let's extract width
 	whRe := regexp.MustCompile(`VK.Widgets.Post\(.+?{width: (\d+)(?:, height: (\d+))?}\)`)
--- turboamper_test.go
+++ turboamper_test.go
@@ -5,6 +5,29 @@ import (
 	"testing"
 )
 
+func TestFBToAMPTable(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  string
+	}{
+		{
+			`<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fstcnk%2Fposts%2F3384458724928901&width=500" width="500" height="498" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>`,
+			`<amp-facebook layout="responsive" width="500" height="498" data-href="https://www.facebook.com/stcnk/posts/3384458724928901"></amp-facebook>`,
+		},
+		{
+			`<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fnasaearth%2Fvideos%2F456540998570328%2F&show_text=0&width=560" width="560" height="373" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>`,
+			`<amp-facebook layout="responsive" width="560" height="373" data-embed-as="video" data-href="https://www.facebook.com/nasaearth/videos/456540998570328/"></amp-facebook>`,
+		},
+	}
+
+	for _, test := range tests {
+		if got, _ := FbToAMP([]byte(test.input)); string(got) != test.want {
+			t.Errorf("\nFbToAMP() = %q,\nwant        %q", got, test.want)
+		}
+	}
+
+}
+
 func TestVkToAMPTable(t *testing.T) {
 	var tests = []struct {
 		input string
